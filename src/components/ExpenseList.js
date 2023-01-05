4dc6e39e4822d43dd8097fc719f71d9a2f3c5a01
ohizz <jtsamuel3084@gmail.com>
2023-01-05 10:36:10 +0100

    fontawesome worked

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -16,7 +16,7 @@ return(
  <>
   {comp &&  <li key={uuid.v4()}>
    <small>{name}</small> <span>${cost}</span>
-   <button onClick={handleDel}><i className="fa fa-trash-o"></i></button>
+   <button onClick={handleDel}><i className="fas fa-trash"></i></button>
   </li>}
  </>  
 )
